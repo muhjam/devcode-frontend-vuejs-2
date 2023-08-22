073820567ab83389b3fb721f42f6077dde5c390f
muhjam <muhhjam@gmail.com>
2023-08-22 15:08:13 +0700

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+// TODO: Uncomment baris kode di bawah untuk meng-import axios
+import axios from "axios";
+
+Vue.use(Vuex);
+
+// TODO: Uncomment baris kode di bawah untuk mendapatkan API URL yang akan digunakan untuk mengambil data kontak dari API
+const BASE_URL = "https://api.contact-manager.project.skyshi.io";
+
+export default new Vuex.Store({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {
+    // TODO: Uncomment baris kode di bawah untuk membuat sebuah fungsi yang akan mengambil semua data kontak dari API
+    async getAllContactsData() {
+      return await axios.get(`${BASE_URL}/contacts`).catch((error) => {
+        return error;
+      });
+    },
+  },
+  modules: {},
+});
